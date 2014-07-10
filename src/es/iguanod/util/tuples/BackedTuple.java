81e347e3b6dc3f5b61644d4349bb8e0ff0b758a1
David Rubio Fernández <rubiof.david@gmail.com>
2014-07-10 21:53:58 +0200

    Update license header

--- src/es/iguanod/util/tuples/BackedTuple.java
+++ src/es/iguanod/util/tuples/BackedTuple.java
@@ -5,26 +5,17 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * A copy of the License should accompany this file or the folder or folder
- * hierarchy where this file is located, or should have been provided by whoever
- * or wherever you obtained this file. If that is not the case you may obtain a
+ * A copy of the License should have been provided along with this file, usually
+ * under the name "LICENSE.txt". If that is not the case you may obtain a
  * copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied;
- * and no Contributor shall be liable for damages, including any direct,
- * indirect, special, incidental, or consequential damages of any character
- * arising as a result of this License or out of the use or inability to use
- * the Work (including but not limited to damages for loss of goodwill, work
- * stoppage, computer failure or malfunction, or any and all other commercial
- * damages or losses), even if such Contributor has been advised of the
- * possibility of such damages.
- * 
- * See the License for further details and the specific language governing
- * permissions and limitations under the License.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package es.iguanod.util.tuples;
 
