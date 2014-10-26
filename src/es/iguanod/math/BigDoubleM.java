ced49476e6e5f6f3cdc859c143b2526226caed2a
David Rubio Fernández <rubiof.david@gmail.com>
2014-10-26 19:53:47 +0100

    Add license header to all files

--- src/es/iguanod/math/BigDoubleM.java
+++ src/es/iguanod/math/BigDoubleM.java
@@ -1,3 +1,22 @@
+/*
+ * -------------------- DO NOT REMOVE OR MODIFY THIS HEADER --------------------
+ * 
+ * Copyright (C) 2014 The Iguanod Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * A copy of the License should have been provided along with this file, usually
+ * under the name "LICENSE.txt". If that is not the case you may obtain a
+ * copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package es.iguanod.math;
 
 import java.math.BigDecimal;
