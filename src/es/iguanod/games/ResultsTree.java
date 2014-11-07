8e98df8451d1fb9f3b771351e75074890fe8e56f
David Rubio Fernández <rubiof.david@gmail.com>
2014-11-07 02:48:15 +0100

    Change ResultsTree to use generic parameter instead of String

--- src/es/iguanod/games/ResultsTree.java
+++ src/es/iguanod/games/ResultsTree.java
@@ -22,6 +22,7 @@ package es.iguanod.games;
 import es.iguanod.base.StringFormatException;
 import es.iguanod.collect.AbstractTree;
 import es.iguanod.collect.AscendingTree;
+import es.iguanod.collect.CollectionsIg;
 import es.iguanod.collect.DoubleTreeCounter;
 import es.iguanod.collect.DoubleTreeCounter.DoubleTreeCounterBuilder;
 import es.iguanod.collect.IntHashCounter;
@@ -33,31 +34,32 @@ import java.util.List;
 import java.util.Map.Entry;
 
 /**
+ * WARNING: games not needed to be played are shown as hyphens.
  *
  * @author <a href="mailto:rubiof.david@gmail.com">David Rubio Fernández</a>
  * @since 1.0.1
  * @version 1.0.1
  */
-public class ResultsTree extends AscendingTree<String[]>{
+public class ResultsTree<T> extends AscendingTree<T[]>{
 
 	private static final long serialVersionUID=1816100213659613130L;
 	//************
 	private IntHashCounter<Integer> spaces=new IntHashCounterBuilder<Integer>().build();
-	private DoubleTreeCounter<String> sons_results=new DoubleTreeCounterBuilder<String>().reverse(true).build();
+	private DoubleTreeCounter<T> sons_results=new DoubleTreeCounterBuilder<T>().reverse(true).build();
 	private int prune_lvls;
 
-	protected static class ResultsTNode extends LinkedTNode<String[]>{
+	protected static class ResultsTNode<T> extends LinkedTNode<T[]>{
 
 		private static final long serialVersionUID=-2184813009761211777L;
 		//************
-		public ArrayList<LinkedTNode<String[]>> rsons=null;
+		public ArrayList<LinkedTNode<T[]>> rsons=null;
 		private static final ArrayList EMPTY_SONS=new ArrayList(0);
 
-		public ResultsTNode(Maybe<? extends String[]> value, LinkedTNode<String[]> parent, AbstractTree<String[]> tree){
+		public ResultsTNode(Maybe<? extends T[]> value, LinkedTNode<T[]> parent, AbstractTree<T[]> tree){
 			super(value, parent, tree);
 		}
 
-		private void fill(List<ResultsTNode> nodes){
+		private void fill(List<ResultsTNode<T>> nodes){
 			this.sons.addAll(nodes);
 		}
 
@@ -66,12 +68,12 @@ public class ResultsTree extends AscendingTree<String[]>{
 			if(sons.isEmpty()){
 				return true;
 			}else{
-				if(((ResultsTNode)sons.get(0)).prune()){
+				if(((ResultsTNode<T>)sons.get(0)).prune()){
 					rsons=sons;
 					sons=EMPTY_SONS;
 				}else{
 					for(int i=1; i < sons.size(); i++){
-						((ResultsTNode)sons.get(i)).prune();
+						((ResultsTNode<T>)sons.get(i)).prune();
 					}
 				}
 				return false;
@@ -80,18 +82,18 @@ public class ResultsTree extends AscendingTree<String[]>{
 	}
 
 	private Iterable<? extends TreeNode> getChildren(TreeNode node){
-		if(((ResultsTNode)node).rsons == null){
+		if(((ResultsTNode<T>)node).rsons == null){
 			return this.children(node);
 		}else{
-			return ((ResultsTNode)node).rsons;
+			return ((ResultsTNode<T>)node).rsons;
 		}
 	}
 
 	private List<? extends TreeNode> getChildrenCopy(TreeNode node){
-		if(((ResultsTNode)node).rsons == null){
+		if(((ResultsTNode<T>)node).rsons == null){
 			return this.childrenCopy(node);
 		}else{
-			return ((ResultsTNode)node).rsons;
+			return ((ResultsTNode<T>)node).rsons;
 		}
 	}
 
@@ -115,21 +117,22 @@ public class ResultsTree extends AscendingTree<String[]>{
 	}
 
 	@Override
-	protected ResultsTNode provideNode(Maybe<? extends String[]> value, LinkedTNode<String[]> parent){
-		return new ResultsTNode(value, parent, this);
+	protected ResultsTNode<T> provideNode(Maybe<? extends T[]> value, LinkedTNode<T[]> parent){
+		return new ResultsTNode<>(value, parent, this);
 	}
 
+	/**
+	 * WARNING: primitive arrays.
+	 *
+	 * @param results
+	 *
+	 * @return
+	 */
 	@Override
-	public TreeNode push(String[] results){
-
-		for(String result:results){
-			if(result.contains("-")){
-				throw new StringFormatException("Hyphens not allowed");
-			}
-		}
+	public TreeNode push(T... results){
 
 		if(prune_lvls > 0 && root != null && this.isFull(root) && this.height() == prune_lvls + 1){
-			((ResultsTNode)this.root).prune();
+			((ResultsTNode<T>)this.root).prune();
 		}
 
 		TreeNode node=super.push(results);
@@ -144,27 +147,27 @@ public class ResultsTree extends AscendingTree<String[]>{
 		for(TreeNode son:getChildren(parent)){
 			sons_results.balancedSum(getValue(son).get());
 		}
-		Entry<String, Double>[] sorted=sons_results.entrySet().toArray(new Entry[]{});
+		Entry<T, Double>[] sorted=CollectionsIg.toGenericArray(sons_results.entrySet());
 
 		if((sorted.length == 1 && sorted[0].getValue() > this.maxSons() / 2.0)
 		|| (sorted.length > 1 && sorted[0].getValue() > sorted[1].getValue() + this.maxSons() - childrenSize(parent))){
-			ArrayList<ResultsTNode> list=new ArrayList<>();
+			ArrayList<ResultsTNode<T>> list=new ArrayList<>();
 			for(int i=childrenSize(parent); i < this.maxSons(); i++){
-				list.add(provideNode(Maybe.from(new String[]{"-"}), (ResultsTNode)parent));
+				list.add(provideNode(Maybe.from((T[])null), (ResultsTNode<T>)parent));
 			}
-			((ResultsTNode)parent).fill(list);
-			this.push(new String[]{sorted[0].getKey()});
+			((ResultsTNode<T>)parent).fill(list);
+			this.push(sorted[0].getKey());
 		}else if(isFull(parent)){
-			ArrayList<String> tmp=new ArrayList<>();
+			ArrayList<T> tmp=new ArrayList<>();
 			double highest=sorted[0].getValue();
-			for(Entry<String, Double> ch:sorted){
+			for(Entry<T, Double> ch:sorted){
 				if(ch.getValue() == highest){
 					tmp.add(ch.getKey());
 				}else{
 					break;
 				}
 			}
-			this.push(tmp.toArray(new String[]{}));
+			this.push(CollectionsIg.toGenericArray(tmp));
 		}
 
 		return node;
@@ -187,12 +190,16 @@ public class ResultsTree extends AscendingTree<String[]>{
 	private void calculateSpaces(TreeNode node, int depth){
 
 		int acc=0;
-		Maybe<String[]> value=getValue(node);
+		Maybe<T[]> value=getValue(node);
 		if(value.isPresent()){
-			for(String str:value.get()){
-				acc+=str.length();
+			if(value.get() != null){
+				for(T str:value.get()){
+					acc+=str.toString().length();
+				}
+				acc+=value.get().length;
+			}else{
+				acc+=2;
 			}
-			acc+=value.get().length;
 		}else{
 			acc=1;
 		}
@@ -236,9 +243,13 @@ public class ResultsTree extends AscendingTree<String[]>{
 			int height=this.pvtHeight(node);
 
 			String results_str="";
-			String[] results=getValue(node).get();
-			for(String result:results){
-				results_str+=result + " ";
+			T[] results=getValue(node).get();
+			if(results == null){
+				results_str+="- ";
+			}else{
+				for(T result:results){
+					results_str+=result + " ";
+				}
 			}
 
 			if(!this.getChildren(node).iterator().hasNext()){
